b8f3eaa37af4e11ecc5c3748859a38f88fc3874a
Hallyson Miranda <hallysonjean@hotmail.com>
2020-07-25 12:13:56 -0300

    funcionalidade final

--- script.js
+++ script.js
@@ -1,29 +1,58 @@
 var map,searchBox,geocoder ;
 var marker = null;
 var input_search = document.getElementById('search'); //input que recebe o nome da cidade ou rua
-var input_numero_residencia = document.getElementById('input-numero'); //input que recebe o número da localização
-var long_lat;
+var input_number_residency = document.getElementById('input-numero'); //input que recebe o número da localização
+
 
 //função que instancia o mapa
 function createMap () {
-  var options = {
-    center: { lat: 43.654, lng: -79.383 },
-    zoom: 15,
-  };
-  map = new google.maps.Map(document.getElementById('map'), options);//inicializando mapa
-  searchBox = new google.maps.places.SearchBox(input_search); //pesquisando o nome da cidade digitada na api
+    var options = {
+      center: new google.maps.LatLng(38.50, -90.50),
+      zoom: 15,
+      draggable: true,
+      gestureHandling: 'greedy',
+      zoomControl: true,
+      mapTypeControl: true,
+      scaleControl: true,
+      streetViewControl: true,
+      rotateControl: true,
+      fullscreenControl: true,
+    };  
+    map = new google.maps.Map(document.getElementById('map'), options);//inicializando mapa
+    searchBox = new google.maps.places.SearchBox(input_search); //pesquisando os endereços na api
 
-  map.addListener('bounds_changed', function() {
+    map.addListener('bounds_changed', function() {
     searchBox.setBounds(map.getBounds());
-  });
-   
-  verificaEndereco();// função onde contém o listener do input de pesquisa dos endereços
+    });
+    //listener chamado quando move clica no mouse e move o mapa
+    map.addListener('mouseup', function() {
+      var coord_center = map.getCenter()
+      geocodeLatLng(coord_center);
+      insertMarkerInMap(coord_center);//mostra o marker
+    });
+    
+    verifyAddress();// função onde contém o listener do input de pesquisa dos endereços
+    geocoder = new google.maps.Geocoder();
 }  
+
+//busca o endereço pelas coordenadas
+function geocodeLatLng(coord) {
+  geocoder.geocode({'location': coord}, function(results, status) {
+    if (status === 'OK') {
+      if (results[0]) {
+        alert(JSON.stringify(results[0].formatted_address))
+      } else {
+        window.alert('No results found');
+      }
+    } else {
+      window.alert('Geocoder failed due to: ' + status);
+    }
+  });
+}
+
 //verifica endereço selecionado e insere o marker no mapa
-function verificaEndereco(){
-  
+function verifyAddress(){
     searchBox.addListener('places_changed', function () {
-      
     var places = searchBox.getPlaces();
 
     if (places.length == 0) return;
@@ -31,64 +60,48 @@ function verificaEndereco(){
     var bounds = new google.maps.LatLngBounds();
     //listando endereços
     places.forEach(function(p) {
-      if (!p.geometry)
-        return;
-        //verifica se o texto da busca está contido o número do endereço
-        if(p.types[0] !== "street_address"){
+          if (!p.geometry)
+          return;
+          console.log(p)
+          //verifica se o texto da busca é um endereço completo com número
+          if(p.types[0] !== "street_address"){
           alert('por favor insira o número da rua')
-        }else{
+
+          }else{
           try {
-            long_lat = p.geometry.location;
-            marker = new google.maps.Marker({
-            map: map,
-            position: long_lat,//lat e long pra salvar no banco
-            animation: google.maps.Animation.DROP,
-            title:'Sua localização',
-            draggable: true,
-          });
-          ouvindoMovimentoMarker()
+          insertMarkerInMap(p.geometry.location); //mostrando o marker
           if (p.geometry.viewport)
             bounds.union(p.geometry.viewport);
           else
             bounds.extend(p.geometry.location);
             map.fitBounds(bounds);
           } catch (error) {
-            
+            console.log(error)
           }
         }
        
     });
   });
 }
-//função chamada quando o número não é inserido junto com o nome da rua, busca a longitude e latitude e insere o marker
-function buscaLocalizacaoEnderecoNumero(){
-  long_lat = null;
-  geocoder = new google.maps.Geocoder();
-  //address : nome da rua + numero da rua
-  var address = input_search.value + input_numero_residencia.value;
 
-  geocoder.geocode( { 'address': address}, function(results, status) {
-    if (status == 'OK') {
-      long_lat = results[0].geometry.location;
-      marker = new google.maps.Marker({
-          map: map,
-          position: long_lat,//lat e long pra salvar no banco
-          animation: google.maps.Animation.DROP,
-          title:'Sua localização',
-          draggable: true,
-      });
-      map.setCenter(long_lat);
-      ouvindoMovimentoMarker()
-    } else {
-      alert('Geocode was not successful for the following reason: ' + status);
-    }
-  });
+//função chamada quando o número não é inserido junto com o nome da rua, busca a longitude e latitude e insere o marker
+function searchLocaleWithNumber(){
+          var address = input_search.value + input_number_residency.value;//address : nome da rua + numero da rua
+          geocoder.geocode( { 'address': address}, function(results, status) {
+          if (status == 'OK') {
+            //results[0].geometry.location = coordenadas do endereço inserido
+          insertMarkerInMap(results[0].geometry.location);//inserindo o marker no mapa
+          } else {
+          alert('Geocode was not successful for the following reason: ' + status);
+          }
+          });
 }
 
-function ouvindoMovimentoMarker(){
-      //ouvindo o mover do marker
-      google.maps.event.addListener(marker, 'dragend', function() 
-      {
-          alert(JSON.stringify(marker.getPosition()));
-      });
+//função reponsavel por inserir o marker no mapa
+function insertMarkerInMap(position){
+    document.getElementById('insertMarkerInMap').style.display = 'inline-block';
+    map.setCenter(position);
+    map.addListener('zoom_changed',function(){
+      map.panTo(position);
+    }) 
 }
